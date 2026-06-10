8c50879ef512877f5007d907522d2d69aa6dc474
Nachiketh1147 <nachikeths1147@gmail.com>
2026-06-10 20:28:44 +0530

    SWagger updated

--- src/main/java/com/hourlyrecruit/security/SwaggerConfig.java
+++ src/main/java/com/hourlyrecruit/security/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.hourlyrecruit.security;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +17,32 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("HourlyRecruit API")
                         .version("1.0"))
+
                 .servers(List.of(
                         new Server().url("https://hourlyrecruit-backend.onrender.com")
-                ));
+                ))
+
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName)
+                )
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
-    
 }
